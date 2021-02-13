970ce3d38efc6f7cdcdfa0cac9b8989ae99c64d1
Otavio <tavim22@hotmail.com>
2021-02-13 10:03:33 -0300

    First Commit

---
+++ project1/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Bettie Butts",
+    age: 27,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg"
+  },
+  {
+    id: 2,
+    name: "Hogan Heagun",
+    age: 23,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg"
+  },
+  {
+    id: 3,
+    name: "Lenny Lonny",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg"
+  },
+  {
+    id: 4,
+    name: "Shian Shittake",
+    age: 42,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
+  },
+  {
+    id: 5,
+    name: "Lola Lila",
+    age: 33,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg"
+  }
+];
